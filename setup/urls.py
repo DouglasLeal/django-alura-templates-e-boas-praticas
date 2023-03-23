4b4910e89621af19043c42c4e4c661431825070d
Douglas Leal <douglas.leal@live.com>
2023-03-23 17:27:28 -0300

    Isolando a URL

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from galeria.views import index
+from django.urls import path, include
 
 urlpatterns = [
-    path('', index),
+    path('', include('galeria.urls')),
     path('admin/', admin.site.urls),
 ]
